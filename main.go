e0b85b7526f98a4212b0a8c0e78ead7fa672a576
aji.karuniadi <ajikaruniadi97@gmail.com>
2021-06-15 06:14:40 +0700

    finish tutorial part 1

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{
+			Title:   "Test Titel",
+			Desc:    "Test Description",
+			Content: "Hello Go",
+		},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "home page endpoint hit")
@@ -12,10 +33,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8001", nil))
 }
 
 func main() {
 	handleRequest()
-	dd()
 }
